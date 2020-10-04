e4475e1a545b08609bd3c0510cf388930ba7f4f8
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2020-10-04 23:19:15 +0530

    Update pattern-01.py

--- pattern-01.py
+++ pattern-01.py
@@ -1,5 +1,6 @@
-for i in range(5):
-    for j in range(5):
+n= int(input('ENTER THE NUMBER OF ROWS YOU WANT : '))
+for i in range(n):
+    for j in range(n):
         print('*',end='')
     print()
 
